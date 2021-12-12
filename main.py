9c6e57785fd140622ff78f7285f75c954238ca0c
Akash Sukhadia <sukhadia_arpit@yahoo.com>
2021-12-12 17:08:17 +0000

    added dropdown and datepicker

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ import plotly.graph_objects as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
 nse = Nse()
+app = dash.Dash(__name__)
 
 def equity_history_virgin(symbol,series,start_date,end_date):
     url="https://www.nseindia.com/api/historical/cm/equity?symbol="+symbol+"&series=[%22"+series+"%22]&from="+str(start_date)+"&to="+str(end_date)+""
     payload = nsefetch(url)
-    res = pd.json_normalize(payload, record_path =['data'])
-    for index, row in res.iterrows():
-        print(row['CH_TRADE_HIGH_PRICE'], row['CH_SYMBOL'])
+    print("url : ", url)
     return pd.json_normalize(payload, record_path =['data'])
 
 def equity_history(symbol,series,start_date,end_date):
@@ -76,26 +76,69 @@ def equity_history(symbol,series,start_date,end_date):
 #    print(total)
     return total
 
-dataset = pd.read_csv("C:\\Users\\HP\\Desktop\\Data.csv", usecols= ['Company Name','Symbol'])
-line_count = 0
-historic_data = equity_history('SBIN', 'EQ', '08-06-2020', '14-06-2021')
+@app.callback(
+    Output("dd-output-container", "figure"),
+    Input("demo-button", "n_clicks"),
+    Input("demo-dropdown", "value"),
+    Input("my-date-picker-range", "start_date"),
+    Input("my-date-picker-range", "end_date")
+)
+def plot_graph(n_clicks, value, start_date, end_date):
+    print("start_date", start_date)
+    start_arr = start_date.split("-")
+    print("start_arr", start_arr)
+    start_date = datetime.datetime(int(start_arr[0]), int(start_arr[1]), int(start_arr[2])).strftime("%d-%m-%Y")
+    print("start_date", start_date)
+    end_date = end_date.split("T")[0]
+    end_arr = end_date.split("-")
+    end_date = datetime.datetime(int(end_arr[0]), int(end_arr[1]), int(end_arr[2])).strftime("%d-%m-%Y")
+    print("end_date", end_date)
+    historic_data = equity_history(value, 'EQ', str(start_date), str(end_date))
 #   for row in dataset.itertuples():
 #    print(row)
 #    print("Data For ", row[1], "is : ", nse.get_quote(row[2]))
 
-fig = go.Figure(data=[
-        go.Candlestick(
-                x=historic_data['CH_TIMESTAMP'],
-                open=historic_data['CH_OPENING_PRICE'],
-                high=historic_data['CH_TRADE_HIGH_PRICE'],
-                low=historic_data['CH_TRADE_LOW_PRICE'],
-                close=historic_data['CH_CLOSING_PRICE']
-                )
-        ])
-fig.update_layout(xaxis_rangeslider_visible=False)
-app = dash.Dash()
+    fig = go.Figure(data=[
+            go.Candlestick(
+                    x=historic_data['CH_TIMESTAMP'],
+                    open=historic_data['CH_OPENING_PRICE'],
+                    high=historic_data['CH_TRADE_HIGH_PRICE'],
+                    low=historic_data['CH_TRADE_LOW_PRICE'],
+                    close=historic_data['CH_CLOSING_PRICE']
+                    )
+            ])
+    fig.update_layout(xaxis_rangeslider_visible=False)
+    return fig
+    
+
+
+dataset = pd.read_csv("C:\\Users\\HP\\Desktop\\Data.csv", usecols= ['Company Name','Symbol'])
+now = datetime.datetime.now()
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[{'label':row[1], 'value':row[2]} for row in dataset.itertuples()],
+        persistence= True
+    ),
+    dcc.DatePickerRange(
+        id='my-date-picker-range',
+        calendar_orientation='vertical',
+        display_format='DD-MM-YYYY',
+#        min_date_allowed=date(2001, 1, 1),
+#        max_date_allowed=date(now.year, now.month, now.day),
+#        initial_visible_month=date(now.year, now.month, now.day),
+        end_date=datetime.datetime(now.year, now.month, now.day),
+        persistence= True,
+        persisted_props= ["start_date", "end_date"]
+    ),
+    html.Button('Submit',
+        id='demo-button',
+        n_clicks=0
+    ),
+    dcc.Graph(
+        id='dd-output-container'
+    )
 ])
+   
 
 app.run_server(debug=True, use_reloader=False) 
